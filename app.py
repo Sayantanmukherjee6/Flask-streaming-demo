bfd14220c30e66d534a4ec6d5b17221bbdd06954
Sayantan Mukherjee <sayantanmukherjee6@gmail.com>
2019-05-06 01:47:02 +0530

    Added vis.js network. Dynamically change color based on the task complete done.

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask, jsonify,request,render_template
 from flask_socketio import send, emit
 from flask_socketio import SocketIO
+import json
 
 app = Flask(__name__,template_folder='templates')
 socketio =SocketIO(app,namespaces=['/recieve_events'])
+
+
+node_list= [
+        {'id': 0, 'label': 'Node 0','shape': 'box'},
+        {'id': 1, 'label': 'Node 1','shape': 'box'},
+        {'id': 2, 'label': 'Node 2','shape': 'box'},
+        {'id': 3, 'label': 'Node 3','shape': 'box'},
+        {'id': 4, 'label': 'Node 4','shape': 'box'},
+        {'id': 5, 'label': 'Node 5','shape': 'box'},
+        {'id': 6, 'label': 'Node 6','shape': 'box'},
+        {'id': 7, 'label': 'Node 7','shape': 'box'},
+        {'id': 8, 'label': 'Node 8','shape': 'box'},
+        {'id': 9, 'label': 'Node 9','shape': 'box'}
+    ]
+
+def data_process(data):
+
+	for val,node in enumerate(node_list):
+		if int(node['id'])==int(data['K']):
+			node['color']='#7BE141'
+			node_list[val]=node
+	return node_list
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -12,13 +35,11 @@ def index():
 @socketio.on('connect',namespace='/recieve_events')
 @app.route('/recieve_events',methods=['GET','POST'])
 def recieve_events():
-    data=request.get_json()    
-    socketio.emit('message',data)    
+    data=request.get_json()
+    d=data_process(data)
+    socketio.emit('message',json.dumps(d))    
     return jsonify({'success':"recieved: "+str(data)}),200
 
-@socketio.on('disconnect',namespace='/recieve_events')
-def disconnect():
-    print('Client disconnected {}'.format(''))
-    
+   
 if __name__ == '__main__':
     socketio.run(app,port=5000,host='0.0.0.0')
